e095f984bdf32e8ee5dd94756247ad481ddb41f4
Felipe Lopez Rios <felipetomaslopezrios@gmail.com>
2023-03-08 19:24:51 -0300

    Todo funcionando

--- Staff/views.py
+++ Staff/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def inicio(request):
+    return HttpResponse("Esta es la pagina de inicio.")
+
+
+def index(request):
+    return render(request, 'Staff/index.html')
