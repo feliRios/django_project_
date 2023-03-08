a8370022b61affc25e7c7dd7569804f03a6116b1
Felipe Lopez Rios <felipetomaslopezrios@gmail.com>
2023-03-08 20:36:01 -0300

    Se crea el panel de administracion

--- Staff/admin.py
+++ Staff/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from Staff.models import ProfesYTutores, OtrosEmpleados
 
 # Register your models here.
+
+admin.site.register(ProfesYTutores)
+admin.site.register(OtrosEmpleados)
